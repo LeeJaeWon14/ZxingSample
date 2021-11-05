e6ee80dce318c99d76398d378556f9fc82e9f716
LeeJaeWon <zhtm2@openobject.net>
2021-11-05 18:08:05 +0900

    - add qr code share

--- app/src/main/java/com/example/zxingsample/CreateActivity.kt
+++ app/src/main/java/com/example/zxingsample/CreateActivity.kt
@@ -1,15 +1,25 @@
 package com.example.zxingsample
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.MediaStore
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.graphics.drawable.toBitmap
 import com.example.zxingsample.databinding.ActivityCreateBinding
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.io.ByteArrayOutputStream
 import java.lang.Exception
 
 class CreateActivity : AppCompatActivity() {
@@ -18,6 +28,7 @@ class CreateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         val bundle = intent.getBundleExtra("dataBundle")
         bundle?.let {
@@ -26,7 +37,7 @@ class CreateActivity : AppCompatActivity() {
             Toast.makeText(this@CreateActivity, "Data를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
             Handler(Looper.getMainLooper()).postDelayed(Runnable {
                 onBackPressed()
-            }, 2000)
+            }, 500)
         }
     }
 
@@ -39,5 +50,40 @@ class CreateActivity : AppCompatActivity() {
         } catch (e: Exception) {
             // no-op
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_create, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_share -> {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                val bitmap = binding.ivQr.drawable.toBitmap()
+                val uri = getImageUri(this, bitmap)
+                shareIntent.setType("image/*")
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                startActivity(Intent.createChooser(shareIntent, "Shared QR code"))
+            }
+            R.id.menu_back -> {
+                finish()
+            }
+        }
+
+        return true
+    }
+
+    private fun getImageUri(context: Context, inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path: String = MediaStore.Images.Media.insertImage(
+            context.getContentResolver(),
+            inImage,
+            "Title",
+            null
+        )
+        return Uri.parse(path)
     }
 }
